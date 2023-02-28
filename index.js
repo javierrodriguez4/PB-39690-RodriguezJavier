2274291c43c4fa3093eee5ec8c12a46769e4a4e0
javierrodriguez4 <javierrodriguez4@outlook.com>
2023-02-28 20:28:43 -0300

    se agregan metodos

--- index.js
+++ index.js
@@ -3,5 +3,35 @@ class ProductManager{
     constructor() {
         this.products = [];
     }
+ 
+    addProduct = (producto, precio, imgPath, productId, stock) => {
+        const product = {
+            id: 0,
+            title: producto,
+            price: precio,
+            thumbnail: imgPath,
+            code: productId,
+            stock: stock,
+        };
     
+    if (this.products.findIndex((product) => product.code === productId)) {
+        if (this.products.length === 0) {
+          product.id = 1;
+        } else {
+          product.id = this.products[this.products.length - 1].id + 1;
+        }
+        this.products.push(product);
+        }
+    }
+    getProducts = () => {
+            return this.products;
+          };
+    getProductById = (id) => {
+            const res = this.products.filter((product) => product.id === id);
+            // console.log(res);
+            if (res.length === 0) {
+              return 'Product not Found';
+            }
+            return res;
+    };
 }
