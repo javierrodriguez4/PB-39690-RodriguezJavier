5f2c2dd9e74438074ade075c6d0dc71f8ef0161c
javierrodriguez4 <javierrodriguez4@outlook.com>
2023-03-01 16:27:08 -0300

    se agrega productos.json

--- index.js
+++ index.js
@@ -1,37 +1,85 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { json } = require("stream/consumers");
+const util = require("util");
+
+const ruta = path.join(`${__dirname}/productos.json`)
+
 class ProductManager{
 
     constructor() {
         this.products = [];
     }
  
-    addProduct = (producto, precio, imgPath, productId, stock) => {
-        const product = {
-            id: 0,
-            title: producto,
-            price: precio,
-            thumbnail: imgPath,
-            code: productId,
-            stock: stock,
-        };
+    async addProduct(producto, precio, imgPath, productId, stock) {
+        try{
+            const {producto, precio, imgPath, productId, stock} = product;
+            this.id= 0;
+            this.title= producto;
+            this.price= precio;
+            this.thumbnail= imgPath;
+            this.code= productId;
+            this.stock= stock;
+
+            if (this.products.findIndex((product) => product.code === productId)) {
+                if (this.products.length === 0) {
+                  product.id = 1;
+                } else {
+                  product.id = this.products[this.products.length - 1].id + 1;
+                }
+                this.products.push(product);
+                }
+            
+            
+                
+        }catch(error){
+            console.log(error)
+        }
+    };
     
-    if (this.products.findIndex((product) => product.code === productId)) {
-        if (this.products.length === 0) {
-          product.id = 1;
-        } else {
-          product.id = this.products[this.products.length - 1].id + 1;
+
+    async getProducts() {
+        try {
+            const info = await fs.readFile(ruta)
+            return JSON.parse(info);
+        }catch (error){
+            console.log(error)
         }
-        this.products.push(product);
-        }
-    }
-    getProducts = () => {
-            return this.products;
           };
     getProductById = (id) => {
             const res = this.products.filter((product) => product.id === id);
-            // console.log(res);
             if (res.length === 0) {
               return 'Product not Found';
             }
             return res;
+        }
+    }
+
+//Test
+
+
+const Producto = async () => {
+
+try{
+    console.log("ejecutando producto")
+    const test = new ProductManager()
+    const listaProductos = await test.getProducts();
+    console.log(listaProductos)
+
+    const productoPrueba ={
+            id: 1,
+            title: "iphone 11 pro",
+            price: 500,
+            thumbnail: "/home/iphone11",
+            code: 23123,
+            stock: 2
     };
+
+    const nuevoProducto = test.getProducts(productoPrueba);
+
+}catch(error) {
+    console.log(error)
 }
+}
+
+Producto()
